913ea261542fe4b9b4042daa17617ed4ed56d1dc
임태한 <dlaxoghks00@naver.com>
2016-09-23 16:01:53 +0900

    A01.java 수정

--- A01.java
+++ A01.java
@@ -1,3 +1,29 @@
+import java.util.*;
+
 class A01 {
 
+	int age = 0;
+
+	public static void main(String[] s){
+	
+		new A01.start();
+	
+	}
+
+	void start() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("태어난 년도를 입력하세요 : ");
+		int year = s.nextInt();
+		this.age = 2016 - year + 1;
+		cal();
+	}
+
+	void cal() {
+
+		if(this.age < 20)
+			System.out.println("미성년자입니다.");
+		else
+			System.out.println("미성년자가 아닙니다.");
+
+	}
 }
